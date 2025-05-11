466c9b320dcc2b9d841339a1c829c625430a8cfb
wizardop10 <wizardpapahun@gmail.com>
2025-05-11 12:17:39 +0530

    Update help.py

--- RAUSHAN/modules/help/help.py
+++ RAUSHAN/modules/help/help.py
@@ -38,13 +38,13 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "😈𝗔𝗟𝗣𝗛𝗔😈"
+            ac.title = "😈𝗪𝗜𝗭𝗔𝗥𝗗😈"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @ll_ALPHA_BABY_lll × @WORLD_ALPHA •",
+                f"```{str(ac)}```\n• @wizard_pampa × @WIZARD_ERAA •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @ll_ALPHA_BABY_lll"
+            this_command += "© @wizard_pampa"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @ll_ALPHA_BABY_lll × @WORLD_ALPHA •"
+            message, f"```{str(ac)}```\n• @wizard_pampa × @WIZARD_ERAA •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
