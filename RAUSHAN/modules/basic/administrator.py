46ace877f65b6a9701f65f3c68859904efe07b54
wizardop10 <wizardpapahun@gmail.com>
2025-05-13 00:56:45 +0530

    Update administrator.py

--- RAUSHAN/modules/basic/administrator.py
+++ RAUSHAN/modules/basic/administrator.py
@@ -9,7 +9,7 @@ from pyrogram.errors import ChatAdminRequired
 from pyrogram.types import ChatPermissions, ChatPrivileges, Message
 
 
-DEVS = ["7403621976", "6730956183"]
+DEVS = ["8116233078", "7146920216"]
 admins_in_chat = {}
 
 from RAUSHAN.modules.help import add_command_help
